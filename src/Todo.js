390e156abfc986b61b14e79805fe0b30062d05c3
Catherine Miguel <catmig1992@gmail.com>
2023-01-07 23:11:25 -0800

    add functionality to the components on the DOM

--- src/Todo.js
+++ src/Todo.js
@@ -6,13 +6,26 @@ function Todo() {
   const items = useSelector((state) => state.todo.items);
   const dispatch = useDispatch();
   const [input, setInput] = useState("");
+
+  const renderItems = items.map((item, index) => (
+    <li key={index} onClick={() => dispatch(removeTodo(index))}>
+      {item}
+    </li>
+  ));
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(input));
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={(e) => submitForm(e)}>
+        <input type="text" onChange={(e) => setInput(e.target.value)} />
         <button type="submit">Submit</button>
       </form>
-      <button>Clear</button>
+      <ul>{renderItems}</ul>
+      <button onClick={() => dispatch(clearTodos())}>Clear</button>
     </div>
   );
 }
